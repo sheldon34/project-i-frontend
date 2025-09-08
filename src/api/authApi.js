caedf14fe846417119815e55b9f66223c9e4bc07
sheldon34 <mubokasheldonmuboka@gmail.com>
2025-09-08 23:17:35 +0300

    fix

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/auth';
+const API_BASE_URL = 'https://project-i-backend-3.onrender.com/api/auth';
 
 const authApi = axios.create({
   baseURL: API_BASE_URL,
--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = 'http://localhost:8080/api/product';
+const API_BASE_URL = 'https://project-i-backend-3.onrender.com/api/product';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
